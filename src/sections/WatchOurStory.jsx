16b028a3f9bbdbaab72a9e65952a09fffc6b2627
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-14 17:37:23 +0600

    Venobox implemented on Video part

--- src/sections/WatchOurStory.jsx
+++ src/sections/WatchOurStory.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Venobox from "venobox";
 
 export default function WatchOurStory() {
+  useEffect(() => {
+    new Venobox({
+      autoplay: false,
+      spinner: "wave",
+      selector: ".our-story-video",
+    });
+  }, []);
   return (
     <>
       <div className="full-wrapper story" name="story">
         <div className="wrapper story-content">
-          <i className="flaticon-play-button"></i>
+          <a
+            class="our-story-video"
+            data-vbtype="video"
+            href="https://youtu.be/SUSSl5vO27k"
+          >
+            <i className="flaticon-play-button"></i>
+          </a>
           <h2>WATCH OUR STORY</h2>
         </div>
       </div>
