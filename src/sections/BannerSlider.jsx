bb326107fc71d4715f7af4059461e12906dd7dea
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-14 15:08:42 +0600

    Type JS Added on Hedline text

--- src/sections/BannerSlider.jsx
+++ src/sections/BannerSlider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
+import Typed from "react-typed";
 
 export default function BannerSlider() {
   const settings = {
@@ -16,7 +17,20 @@ export default function BannerSlider() {
       <div className="wrapper banner-content">
         <Slider {...settings}>
           <div>
-            <h1>We Are Awesome Creative Agency</h1>
+            <h1>
+              We{" "}
+              <Typed
+                strings={[
+                  "Are Awesome Creative Agency",
+                  "Make Life Easy",
+                  "Build Dream",
+                  "Value Your Time and Money",
+                ]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              ></Typed>
+            </h1>
             <h2>
               A reader will be distracted by the readable content of a page when
               looking at its layout. The point of using Lorem Ipsum is that it
@@ -27,7 +41,20 @@ export default function BannerSlider() {
             </h2>
           </div>
           <div>
-            <h1>We Make Life Easy</h1>
+            <h1>
+              We{" "}
+              <Typed
+                strings={[
+                  "Make Life Easy",
+                  "Build Dream",
+                  "Value Your Time and Money",
+                  "Are Awesome Creative Agency",
+                ]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              ></Typed>
+            </h1>
             <h2>
               The point of using Lorem Ipsum is that it has a more-or-less
               normal distribution of letters, as opposed to using 'Content here,
@@ -38,7 +65,20 @@ export default function BannerSlider() {
             </h2>
           </div>
           <div>
-            <h1>We Build Dream</h1>
+            <h1>
+              We{" "}
+              <Typed
+                strings={[
+                  "Build Dream",
+                  "Value Your Time and Money",
+                  "Are Awesome Creative Agency",
+                  "Make Life Easy",
+                ]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              ></Typed>
+            </h1>
             <h2>
               As opposed to using 'Content here, content here', making it look
               like readable English. Many desktop publishing packages and web
@@ -49,7 +89,20 @@ export default function BannerSlider() {
             </h2>
           </div>
           <div>
-            <h1>We Value Your Time and Money</h1>
+            <h1>
+              We{" "}
+              <Typed
+                strings={[
+                  "Value Your Time and Money",
+                  "Are Awesome Creative Agency",
+                  "Make Life Easy",
+                  "Build Dream",
+                ]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              ></Typed>
+            </h1>
             <h2>
               Many desktop publishing packages and web page editors now use
               Lorem Ipsum as their default model text.A reader will be
