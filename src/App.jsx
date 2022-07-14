47435d84e13379282f22e8ffc5411ed74b4138bb
Miraz Vai <freelancer.miraz.uddin@gmail.com>
2022-07-14 12:24:33 +0600

    Slick Slider Added on Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import "../node_modules/slick-carousel/slick/slick-theme.css";
+import "../node_modules/slick-carousel/slick/slick.css";
 import "./App.css";
 import Home from "./pages/Home";
 
--- src/sections/BannerSlider.jsx
+++ src/sections/BannerSlider.jsx
@@ -1,44 +1,69 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Slider from "react-slick";
 
 export default function BannerSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  };
   return (
     <div className="full-wrapper banner" name="banner">
       <div className="wrapper banner-content">
-        <h1>We Are Awesome Creative Agency</h1>
-        <h2>
-          This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel
-          velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
-          nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed
-          odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan
-          ipsum velit.
-        </h2>
-        <a href="#">learn more</a>
-        <div className="circle-slide">
-          <div className="slide">
-            <i className="far fa-circle"></i>
-            <div className="slide-over">
-              <i className="fas fa-circle"></i>
-            </div>
+        <Slider {...settings}>
+          <div>
+            <h1>We Are Awesome Creative Agency</h1>
+            <h2>
+              A reader will be distracted by the readable content of a page when
+              looking at its layout. The point of using Lorem Ipsum is that it
+              has a more-or-less normal distribution of letters, as opposed to
+              using 'Content here, content here', making it look like readable
+              English. Many desktop publishing packages and web page editors now
+              use Lorem Ipsum as their default model text.
+            </h2>
           </div>
-          <div className="slide">
-            <i className="far fa-circle"></i>
-            <div className="slide-over">
-              <i className="fas fa-circle"></i>
-            </div>
+          <div>
+            <h1>We Make Life Easy</h1>
+            <h2>
+              The point of using Lorem Ipsum is that it has a more-or-less
+              normal distribution of letters, as opposed to using 'Content here,
+              content here', making it look like readable English. Many desktop
+              publishing packages and web page editors now use Lorem Ipsum as
+              their default model text.A reader will be distracted by the
+              readable content of a page when looking at its layout.
+            </h2>
           </div>
-          <div className="slide">
-            <i className="far fa-circle"></i>
-            <div className="slide-over">
-              <i className="fas fa-circle"></i>
-            </div>
+          <div>
+            <h1>We Build Dream</h1>
+            <h2>
+              As opposed to using 'Content here, content here', making it look
+              like readable English. Many desktop publishing packages and web
+              page editors now use Lorem Ipsum as their default model text.A
+              reader will be distracted by the readable content of a page when
+              looking at its layout.The point of using Lorem Ipsum is that it
+              has a more-or-less normal distribution of letters.
+            </h2>
           </div>
-          <div className="slide">
-            <i className="far fa-circle"></i>
-            <div className="slide-over">
-              <i className="fas fa-circle"></i>
-            </div>
+          <div>
+            <h1>We Value Your Time and Money</h1>
+            <h2>
+              Many desktop publishing packages and web page editors now use
+              Lorem Ipsum as their default model text.A reader will be
+              distracted by the readable content of a page when looking at its
+              layout.The point of using Lorem Ipsum is that it has a
+              more-or-less normal distribution of letters. As opposed to using
+              'Content here, content here', making it look like readable
+              English.
+            </h2>
           </div>
-          <div className="clr"></div>
+        </Slider>
+        <div>
+          {/* <a href="#">Get Connected</a> */}
+          <Link to="/">Get Connected</Link>
         </div>
       </div>
     </div>
